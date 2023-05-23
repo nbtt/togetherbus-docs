41bb822a2aff1b954f359c65a926cc4f666334cb
tuantnb <tuantnb@vng.com.vn>
2023-05-23 07:55:27 +0700

    update price format

--- service/routes.js
+++ service/routes.js
@@ -7,13 +7,13 @@ const routes = function(mock) {
                 {
                     "busNo": "08", // bus route number
                     "operatingTime": "10:20 - 20:30", // format: "hh:mm - hh:mm"
-                    "price": 10, // 10 000 VND, unit: 1000 VND
+                    "price": "10000", // 10 000 VND
                     "name": "Bến Xe Buýt Quận 8- Đại học Quốc Gia" // display route name
                 },
                 {
                     "busNo": "60-1",
                     "operatingTime": "05:20 - 10:40",
-                    "price": 7,
+                    "price": "7000",
                     "name": "Test tuyến"
                 }
             ],
